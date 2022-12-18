271c357dea49363c1c94c651f478bb94b1f7757c
Cuong Truong <truonglevancuong92@gmail.com>
2022-12-19 00:01:57 +0700

    Grant permission

--- ios/Classes/InAppWebView/InAppWebView.swift
+++ ios/Classes/InAppWebView/InAppWebView.swift
@@ -1614,6 +1614,18 @@ public class InAppWebView: WKWebView, UIScrollViewDelegate, WKUIDelegate, WKNavi
         
         inAppBrowserDelegate?.didStartNavigation(url: url)
     }
+
+
+    @available(iOS 15, *)
+    public func webView(
+        _ webView: WKWebView,
+        requestMediaCapturePermissionFor origin: WKSecurityOrigin,
+        initiatedByFrame frame: WKFrameInfo,
+        type: WKMediaCaptureType,
+        decisionHandler: @escaping (WKPermissionDecision) -> Void
+    ) {
+        decisionHandler(.grant)
+    }
     
     public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         initializeWindowIdJS()
